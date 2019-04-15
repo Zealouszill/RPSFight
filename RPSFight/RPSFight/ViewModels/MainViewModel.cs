5c04da0fe1b24cbb5f3e94c3b292b2ac3e6176d4
S. Stewart <rubix.this@gmail.com>
2019-04-15 16:13:56 -0600

    Push it

--- RPSFight/RPSFight/ViewModels/MainViewModel.cs
+++ RPSFight/RPSFight/ViewModels/MainViewModel.cs
@@ -23,21 +23,62 @@ namespace RPSFight.ViewModels
         public MainViewModel(RPSDataStorage.Data.DataStoreRepo context)
         {
             string testingString = "test";
-            TestStringFunction = testingString;
+            PlayerRocksFunction = testingString;
 
             dataStore = context;
             Roshamos = new ObservableCollection<Roshamo>();
             //Roshamos = DataStore.GetAllRoshamos();
-            Roshamos.Add(new Roshamo());
+            //Roshamos.Add(new Roshamo());
         }
 
-        private string testString;
-        public string TestStringFunction
+        private string PlayerRocks;
+        public string PlayerRocksFunction
         {
-            get { return testString; }
-            set { SetField(ref testString, value); }
+            get { return PlayerRocks; }
+            set { SetField(ref PlayerRocks, value); }
         }
 
+        private string PlayerPapers;
+        public string PlayerPapersFunction
+        {
+            get { return PlayerPapers; }
+            set { SetField(ref PlayerPapers, value); }
+        }
+
+        private string PlayerScissors;
+        public string PlayerScissorsFunction
+        {
+            get { return PlayerScissors; }
+            set { SetField(ref PlayerScissors, value); }
+        }
+
+        private string EnemyRocks;
+        public string EnemyRocksFunction
+        {
+            get { return EnemyRocks; }
+            set { SetField(ref EnemyRocks, value); }
+        }
+
+        private string EnemyPapers;
+        public string EnemyPapersFunction
+        {
+            get { return EnemyPapers; }
+            set { SetField(ref EnemyPapers, value); }
+        }
+
+        private string EnemyScissors;
+        public string EnemyScissorsFunction
+        {
+            get { return EnemyScissors; }
+            set { SetField(ref EnemyScissors, value); }
+        }
+
+        private string Winner;
+        public string WinnerFunction
+        {
+            get { return Winner; }
+            set { SetField(ref Winner, value); }
+        }
 
         public ICommand StartBattle => startBattle ?? (resultsCommand = new SimpleCommand(
             () =>
@@ -46,6 +87,16 @@ namespace RPSFight.ViewModels
                 Console.WriteLine("We started an attack");
             }
             ));
+
+        //public Roshamo RandomRoshambo()
+        //{
+        //    Roshamo tempRoshambo = new Roshamo();
+
+
+
+        //    return tempRoshambo;
+        //}
+
 
         #region INotifyPropertyChanged Implementation
         public event PropertyChangedEventHandler PropertyChanged;
