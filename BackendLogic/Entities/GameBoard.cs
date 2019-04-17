0cf4223aea5129f98d22a883477a4835290d53ec
Jacob Ashcraft <00151399@badgermail.snow.edu>
2019-04-17 08:22:04 -0600

    Made randomize player.

--- BackendLogic/Entities/GameBoard.cs
+++ BackendLogic/Entities/GameBoard.cs
@@ -16,6 +16,7 @@ namespace RPSBackendLogic.Entities
         private Random rnd;
         private bool[] PlayerWins { get; set; }
         private int[] enemyQ;
+        private int[] playerQ;
         private int count;
         public ObservableCollection<Name> WinLog { get; set; }
 
@@ -34,27 +35,28 @@ namespace RPSBackendLogic.Entities
                 count = 0;
                 PlayerWins = new bool[GameLength];
                 enemyQ = new int[3] { Enemy.Rock.Quantity, Enemy.Paper.Quantity, Enemy.Scissors.Quantity };
-                var playerQ = new int[3] { Player.Rock.Quantity, Player.Paper.Quantity, Player.Scissors.Quantity };
+                playerQ = new int[3] { Player.Rock.Quantity, Player.Paper.Quantity, Player.Scissors.Quantity };
                 for (int i = 0; i < GameLength; i++)
                 {
-                    int participant = NewEnemy();
+                    int player = NewParticipant();
+                    int enemy = NewParticipant(false);
                     // 1, 2, 3 -> 2
-                    if (playerQ[0] != 0)
+                    if (player == 1)
                     {
-                        if (participant == 1)
+                        if (enemy == 1)
                             WinLog.Add("Player Rock vs Enemy Paper - " + Combat(Player.Rock.ModPaper, Enemy.Paper.ModRock).ToString());
 
-                        else if (participant == 2)
+                        else if (enemy == 2)
                             WinLog.Add("Player Rock vs Enemy Scissors - " + Combat(Player.Rock.ModScissor, Enemy.Scissors.ModRock).ToString());
                         else
                             WinLog.Add("Player Rock vs Enemy Paper - " + Combat(0, 0).ToString());
                         playerQ[0] -= 1;
                     }
-                    else if (playerQ[1] != 0)
+                    else if (player == 2)
                     {
-                        if (participant == 0)
+                        if (enemy == 0)
                             WinLog.Add("Player Paper vs Enemy Rock - " + Combat(Player.Paper.ModRock, Enemy.Rock.ModPaper).ToString());
-                        else if (participant == 2)
+                        else if (enemy == 2)
                             WinLog.Add("Player Paper vs Enemy Scissor - " + Combat(Player.Paper.ModScissor, Enemy.Scissors.ModPaper).ToString());
                         else
                             WinLog.Add("Player Paper vs Enemy Paper - " + Combat(0, 0).ToString());
@@ -62,9 +64,9 @@ namespace RPSBackendLogic.Entities
                     }
                     else
                     {
-                        if (participant == 0)
+                        if (enemy == 0)
                             WinLog.Add("Player Scissor vs Enemy Rock - " + Combat(Player.Scissors.ModRock, Enemy.Rock.ModScissor).ToString());
-                        else if (participant == 1)
+                        else if (enemy == 1)
                             WinLog.Add("Player Scissor vs Enemy Scissor - " + Combat(Player.Scissors.ModPaper, Enemy.Paper.ModScissor).ToString());
                         else
                             WinLog.Add("Player Scissor vs Enemy Paper - " + Combat(0, 0).ToString());
@@ -84,22 +86,42 @@ namespace RPSBackendLogic.Entities
             else
                 return Enemy;
         }
-        private int NewEnemy()
+        private int NewParticipant(bool player = true)
         {
-            if(enemyQ[0] != 0)
+            if (!player)
             {
-                enemyQ[0] -= 1;
-                return rnd.Next(1, 4);
-            }
-            else if (enemyQ[1] != 0)
+                if (enemyQ[0] != 0)
+                {
+                    enemyQ[0] -= 1;
+                    return rnd.Next(1, 4);
+                }
+                else if (enemyQ[1] != 0)
+                {
+                    enemyQ[1] -= 1;
+                    return rnd.Next(2, 4);
+                }
+                else
+                {
+                    enemyQ[2] -= 1;
+                    return 3;
+                }
+            }else
             {
-                enemyQ[1] -= 1;
-                return rnd.Next(2, 4);
-            }
-            else
-            {
-                enemyQ[2] -= 1;
-                return 3;
+                if (playerQ[0] != 0)
+                {
+                    playerQ[0] -= 1;
+                    return rnd.Next(1, 4);
+                }
+                else if (playerQ[1] != 0)
+                {
+                    playerQ[1] -= 1;
+                    return rnd.Next(2, 4);
+                }
+                else
+                {
+                    playerQ[2] -= 1;
+                    return 3;
+                }
             }
         }
         private bool Combat(double playerMod, double enemyMod)
